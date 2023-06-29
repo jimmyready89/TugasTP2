fdcebe0fcaa93f626530e69a1e34e9a66b2c4b7b
Rico Samuel <samuelricoss@yahoo.co.id>
2023-06-29 14:05:18 +0700

    model

---
+++ app/Models/User/UserModel.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models\User;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class UserModel extends Model
+{
+    protected $table = 'User';
+    protected $primaryKey = 'id';
+    // protected $primaryKey = 'id';
+    protected $attributes = [
+        'active' => 1,
+        'is_admin' => 0,
+    ];
+    protected $fillable = [
+        'username',
+        'password',
+        'salt',
+        'usercreate_id',
+        'userupdate_id',
+        'active',
+        'is_admin'
+    ];
+}
--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('Layout');
+});
+
+Route::get('/SignIn', function () {
+    return view('SignIn/Login');
 });
