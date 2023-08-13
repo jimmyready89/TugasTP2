2833f942b083bfa606eeb0a25080dde8ef8e940f
Rico Samuel <samuelricoss@yahoo.co.id>
2023-08-13 23:45:15 +0700

    feat: Invoice/EditInvoice
    Edit Invoice from field
    1. no invoice
    2. customer name
    3. date
    
    Issue: Module Invoice #22

---
+++ app/Http/Requests/Invoice/EditInvoiceRequest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Requests\Invoice;
+
+use App\Custome\RequestForm;
+
+class EditInvoiceRequest extends RequestForm
+{
+    public function rules(): array
+    {
+        return [
+            "no_invoice" => ["required", "string"],
+            "customer_name" => ["required", "string"],
+            "date" => ["required", "date_format:Y-m-d"]
+        ];
+    }
+
+    public function message(): array {
+        return [
+            "required" => ":attirbute is required",
+            "date_format" => ":attribute invalid date format"
+        ];
+    }
+
+    public function attributes(): array {
+        return [
+            "no_invoice" => "No Invoice",
+            "customer_name" => "Customer Name",
+            "date" => "Date"
+        ];
+    }
+}
